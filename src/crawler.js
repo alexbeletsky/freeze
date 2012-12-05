eb68df4114f4fb8ff59b427474364e9054830f6f
alexander.beletsky <alexander.beletsky@gmail.com>
2012-12-05 21:30:04 +0200

    one page links extractor

--- src/crawler.js
+++ src/crawler.js
@@ -1,6 +1,29 @@
+var links = require('./links');
+
 var crawler = {
     initialize: function (request) {
         this.request = request;
+        // this.extractor = extractor;
+    },
+
+    links: function (url, callback) {
+        this.request(url, function (err, response, body) {
+            if (err) {
+                return callback (err);
+            }
+
+            if (response.statusCode !== 200) {
+                return callback('request to: ' + url + ' failed. response code: ' + response.statusCode);
+            }
+
+            links.extract(body, function (err, extracted) {
+                if (err) {
+                    return callback('extracting links from ' + url + ' failed.');
+                }
+
+                callback(null, extracted);
+            });
+        });
     }
 };
 
--- test/crawler.spec.js
+++ test/crawler.spec.js
@@ -5,20 +5,49 @@ var crawler = require('../src/crawler');
 
 describe('crawler specs', function () {
 
+    var request;
+
+    beforeEach(function () {
+        request = function (url, callback) {
+            callback();
+        };
+    });
+
+    beforeEach(function () {
+        crawler.initialize(request);
+    });
+
     describe ('when crawler is created', function () {
-
-        var request;
-
-        beforeEach(function () {
-            request = sinon.spy();
-        });
-
-        beforeEach(function () {
-            crawler.initialize(request);
-        });
 
         it ('should be initialized', function () {
             expect(crawler.request).to.be.ok;
+        });
+
+    });
+
+    describe ('when crawler started to extract links', function () {
+        var extractedLinks;
+
+        beforeEach(function () {
+            request = function (url, callback) {
+                callback(null, { statusCode: 200 }, '<a href="test.html">link</a>');
+            };
+        });
+
+        beforeEach(function (done) {
+            crawler.initialize(request);
+            crawler.links('http://target.com', function (err, links) {
+                extractedLinks = links;
+                done();
+            });
+        });
+
+        it ('should extract all links from url', function () {
+            expect(extractedLinks).to.be.ok;
+        });
+
+        it ('should have correct href', function () {
+            expect(extractedLinks[0]).to.equal('test.html');
         });
 
     });

3374016b68dad2970d11abf1b1ddc9c7a3a79f23
alexander.beletsky <alexander.beletsky@gmail.com>
2012-12-08 19:26:09 +0200

    dump dumb

---
+++ src/dump.js
@@ -0,0 +1,5 @@
+var dump = {
+
+};
+
+module.exports = dump;
---
+++ test/dump.spec.js
@@ -0,0 +1,12 @@
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+var dump = require('../src/dump');
+
+describe('dump.js', function () {
+
+    it ('should exist', function () {
+        expect(dump).to.be.ok;
+    });
+
+});
